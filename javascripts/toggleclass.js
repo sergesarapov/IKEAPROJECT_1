7d2f5ac07374b7408249861a70937b446b2f06dc
sergesarapov <sarapov.ss@gmail.com>
2015-06-01 08:57:21 +0300

    добавил проект

---
+++ javascripts/toggleclass.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("section.sidebar").click(function () {
+    $("section.sidebar").toggleClass("show");
+  });
+  $("section.sidebar").click(function () {
+    $("section.sidebar").toggleClass("hovered");
+  });
+});
